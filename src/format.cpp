acdce7c915db3f47252626a7fd430cd537f54cf3
Anshul Sungra <34504695+AnsSUN@users.noreply.github.com>
2022-05-02 22:44:07 -0400

    Adding files

---
+++ src/format.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+#include "format.h"
+
+using std::string;
+
+// TODO: Complete this helper function
+// INPUT: Long int measuring seconds
+// OUTPUT: HH:MM:SS
+// REMOVE: [[maybe_unused]] once you define the function
+string Format::ElapsedTime(long seconds) { 
+  int min = seconds/60;
+  int hr = min/60;
+  string out = std::to_string(int(hr))+ ":" + std::to_string(int(min)) + ":" + std::to_string(int(seconds%60));
+  return out; 
+}
