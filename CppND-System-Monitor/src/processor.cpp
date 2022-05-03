152ad25d2f2617ce77eca274a839ba4c5c2aa93f
Anshul Sungra <34504695+AnsSUN@users.noreply.github.com>
2022-05-03 19:53:55 -0400

    Delete CppND-System-Monitor directory

--- CppND-System-Monitor/src/processor.cpp
+++
@@ -1,7 +0,0 @@
-#include "processor.h"
-#include "linux_parser.h"
-
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { 
-return LinuxParser::ActiveJiffies() / (LinuxParser::ActiveJiffies() + LinuxParser::IdleJiffies());
-}
